8b2dc994857a877624c04e7dce5a3de72b7a789d
oziel <oziel.silva222@gmail.com>
2021-01-07 13:53:52 -0300

    recaptcha

---
+++ seguranca/exemplo04.php
@@ -0,0 +1,6 @@
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<form action="cadastrar.php" method="post">
+  <div class="g-recaptcha" data-sitekey="6LfofiQaAAAAAIlZcPXor-44_81b1G0ii0_iHc2F"></div>
+  <input type="email" name="inputEmail">
+  <button type="submit">Enviar</button>
+</form>
